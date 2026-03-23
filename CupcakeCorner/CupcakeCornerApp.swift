aaf582bd4b247169864cc1bee31aa1e990246f14
Radu Edward-Andrei <raduedwardandrei@gmail.com>
2026-03-23 17:46:09 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Radu Edward-Andrei on 23.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
